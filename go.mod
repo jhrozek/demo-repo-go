00ad496c98755917c27750410526b152a1c45e0a
Jakub Hrozek <jakub@stacklok.com>
2024-05-02 14:26:42 +0200

    Add a GPLv2 library github.com/aclindsa/ofxgo

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/stacklok/demo-repo-go
 
+require github.com/aclindsa/ofxgo v0.1.1
+
+require (
+	github.com/aclindsa/xml v0.0.0-20190701095008-453d2c6090c2 // indirect
+	golang.org/x/text v0.0.0-20180911161511-905a57155faa // indirect
+)
+
 go 1.20
--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/aclindsa/ofxgo"
 )
 
 func main() {
+	_ = ofxgo.BasicClient{}
+
 	// comment
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
